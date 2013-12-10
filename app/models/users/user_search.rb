c4e24f35bb4b7f15df0914f104d4262e0b630f14
Schmidie <2toschmid@googlemail.com>
2013-12-10 12:11:25 +0100

    in direction user search

---
+++ app/models/users/user_search.rb
@@ -0,0 +1,52 @@
+#
+#
+# == License:
+# Fairnopoly - Fairnopoly is an open-source online marketplace.
+# Copyright (C) 2013 Fairnopoly eG
+#
+# This file is part of Fairnopoly.
+#
+# Fairnopoly is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Fairnopoly is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Fairnopoly.  If not, see <http://www.gnu.org/licenses/>.
+#
+module UserSearch
+  extend ActiveSupport::Concern
+
+  included do
+    searchable do
+      text :nickname, :boost => 5.0, :stored => true
+    end
+
+    # Indexing via Delayed Job Daemon
+    handle_asynchronously :solr_index, queue: 'indexing', priority: 50
+    handle_asynchronously :solr_index!, queue: 'indexing', priority: 50
+
+    alias_method_chain :remove_from_index, :delayed
+    alias :solr_remove_from_index :remove_from_index
+
+  end
+
+  def remove_from_index_with_delayed
+    Delayed::Job.enqueue RemoveIndexJob.new(record_class: self.class.to_s, attributes: self.attributes), queue: 'indexing', priority: 50
+  end
+
+  def find_like_this page
+    User.search do
+      fulltext self.nickname do
+          fields(:nickname)
+        end
+      end
+      #paginate :page => page, :per_page => Kaminari.config.default_per_page
+   end
+
+end
